443be4e4402ed3eb123ffc81dd1d4a6a9b4d94ec
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-04-07 11:17:52 -0300

    vista desktop de navbar terminada

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import SideMenu from "../SideMenu/SideMenu";
 
 import styles from "./NavBar.module.css";
+import GradientButton from "../GradientButton/GradientButton";
+import NavLinks from "../NavLinks/NavLinks";
 
 const links: { path: string; label: string }[] = [
 	{
@@ -34,6 +36,15 @@ function NavBar({ logo, alt }: { logo: string; alt: string }) {
 				<Link href={"/"}>
 					<Image src={logo} alt={alt} width={100} height={20} />
 				</Link>
+
+				<div className={styles.desktop}>
+					<NavLinks links={links} />
+				</div>
+
+				<div className={styles.desktop}>
+					<GradientButton>Request Invite</GradientButton>
+				</div>
+
 				<div className={styles.navbutton}>
 					<SideMenu links={links} />
 				</div>
---
+++ src/components/NavLinks/NavLinks.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "./NavLinks.module.css";
+
+function NavLinks({ links }: { links: { path: string; label: string }[] }) {
+	return (
+		<ul className={styles.navlinks}>
+			{links.map((link: { path: string; label: string }, idx) => (
+				<li key={idx}>
+					<Link href={link.path}>{link.label}</Link>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default NavLinks;
