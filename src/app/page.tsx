0b60e9455454fdd8efada03468518dbccb52d526
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-04-10 08:46:29 -0300

    adaptando estilos a desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
 						<div className={styles.mobile}>
 							<Image
 								src={backgroundMobile}
-								alt="mockups"
+								alt="easybank-background"
 								objectFit="cover"
 								objectPosition="center"
 								layout="fill"
@@ -90,7 +90,7 @@ export default function Home() {
 						<div className={styles.desktop}>
 							<Image
 								src={backgroundDesktop}
-								alt="mockups"
+								alt="easybank-background"
 								objectFit="cover"
 								objectPosition="-50px -208px"
 								layout="fill"
