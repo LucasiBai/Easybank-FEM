f74a548197fdc68bc91e955e3e6c0c0d69a1e2e1
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-04-13 06:04:32 -0300

    agregando estilos desktop al footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,35 +16,41 @@ function Footer({
 }) {
 	return (
 		<footer className={styles.footer}>
-			<Link href={"/"}>
-				<Image src={logo} alt={alt} width={100} height={20} />
-			</Link>
+			<span>
+				<div>
+					<Link href={"/"}>
+						<Image src={logo} alt={alt} width={100} height={20} />
+					</Link>
 
-			<ul className={styles.media}>
-				{socialMedia.map((media, idx) => (
-					<li key={idx}>
-						<Link href={media.href} target="_blank">
-							<Image
-								src={media.icon}
-								alt={media.media}
-								width={24}
-								height={24}
-							/>
-						</Link>
-					</li>
-				))}
-			</ul>
+					<ul className={styles.media}>
+						{socialMedia.map((media, idx) => (
+							<li key={idx}>
+								<Link href={media.href} target="_blank">
+									<Image
+										src={media.icon}
+										alt={media.media}
+										width={24}
+										height={24}
+									/>
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
 
-			<ul className={styles.links}>
-				{links.map((link, idx) => (
-					<li key={idx}>
-						<Link href={link.path}>{link.label}</Link>
-					</li>
-				))}
-			</ul>
+				<ul className={styles.links}>
+					{links.map((link, idx) => (
+						<li key={idx}>
+							<Link href={link.path}>{link.label}</Link>
+						</li>
+					))}
+				</ul>
 
-			<GradientButton>Request Invite</GradientButton>
-			<h6>© Easybank. All Rights Reserved</h6>
+				<div>
+					<GradientButton>Request Invite</GradientButton>
+					<h6>© Easybank. All Rights Reserved</h6>
+				</div>
+			</span>
 		</footer>
 	);
 }
